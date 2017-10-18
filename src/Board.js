6b3c53eb66e15c6f7bf5f83e9484f5441ff361b0
Hack Reactor Student <student@hackreactor.com>
2017-10-18 15:45:57 -0700

    Implements conflict detection for rows, columns, and major diaganols

--- src/Board.js
+++ src/Board.js
@@ -74,17 +74,37 @@
     =                 TODO: fill in these Helper Functions                    =
     =========================================================================*/
 
+    // HELPERS
+    // --------------------------------------------------------------
+    //
+    // add up values in an array
+    isConflict: function(arr) {
+      var count = _.reduce(arr, function(sum, val) {
+        return sum + val;
+      });
+      return count > 1;
+    },
+
     // ROWS - run from left to right
     // --------------------------------------------------------------
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var singleRow = this.get(rowIndex);
+      return this.isConflict(singleRow);
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var hasConflict = false;
+      
+      for (var i = 0; i < this.attributes.n; i++) {
+        if (this.hasRowConflictAt(i)) {
+          hasConflict = true;
+        }
+      }
+      
+      return hasConflict;
     },
 
 
@@ -94,12 +114,26 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var colArr = [];
+      
+      for (var i = 0; i < this.attributes.n; i++) {
+        colArr.push(this.get(i)[colIndex]);
+      }
+      
+      return this.isConflict(colArr);
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var hasConflict = false;
+      
+      for (var i = 0; i < this.attributes.n; i++) {
+        if (this.hasColConflictAt(i)) {
+          hasConflict = true;
+        }
+      }
+      
+      return hasConflict;
     },
 
 
@@ -109,12 +143,38 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var diagonal = [];
+      var startingRow, startingCol;
+      
+      if (majorDiagonalColumnIndexAtFirstRow < 0) {
+        startingRow = Math.abs(majorDiagonalColumnIndexAtFirstRow);
+        startingCol = 0;
+      } else {
+        startingRow = 0;
+        startingCol = majorDiagonalColumnIndexAtFirstRow;
+      }
+      
+      for (var i = startingRow; i < this.attributes.n; i++) {
+        var space = this.get(i)[startingCol];
+        if (space === undefined) {
+          break;
+        }
+        diagonal.push(space);
+        startingCol++;
+      }
+      return this.isConflict(diagonal);
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var hasConflict = false;
+      for (var i = -3; i < this.attributes.n; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          hasConflict = true;
+        }
+      }
+      
+      return hasConflict;
     },
 
 
@@ -124,12 +184,12 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
